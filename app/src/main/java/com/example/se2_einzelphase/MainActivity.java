4e88e3d874b54f26c550905a65bc420770465ed2
floratsch <flratschnig@edu.aau.at>
2021-03-08 16:55:15 +0100

    Created View and linked View to Code

--- app/src/main/java/com/example/se2_einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.se2_einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn;
+    private EditText matNo;
+    private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        matNo =  findViewById(R.id.inputMatNo);
+        result =  findViewById(R.id.result);
+
+        btn.setOnClickListener( (View.OnClickListener) this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
