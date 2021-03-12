6e9eb18ed55c4f81f805f83c8b368c6c0e028d12
floratsch <flratschnig@edu.aau.at>
2021-03-12 22:46:14 +0100

    Created TCP Client

---
+++ app/src/main/java/com/example/se2_einzelphase/TCP.java
@@ -0,0 +1,31 @@
+package com.example.se2_einzelphase;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCP extends Thread {
+
+    String sentence;
+    String res;
+
+    @Override
+    public void run() {
+
+        try {
+            Socket socket = new Socket("se2-isys.aau.at", 53212);
+            DataOutputStream toSever = new DataOutputStream(socket.getOutputStream());
+
+            toSever.writeBytes(sentence + '\n');
+
+            BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            res = fromServer.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
